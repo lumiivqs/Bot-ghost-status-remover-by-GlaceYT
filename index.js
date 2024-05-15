14281471f92447e7693a7dd86d8e93dc442a00da
lumiivqs <lumiivqs@gmail.com>
2024-05-15 20:09:23 +1000

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Making your order.","Concept Designs EST. 2024"];
 
 
 let currentIndex = 0;
